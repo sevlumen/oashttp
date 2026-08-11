835742ceda330ba549d33bb351ecee18173110b7
Quang <97324003+quang0201@users.noreply.github.com>
2026-08-11 11:08:54 +0700

    feat: support schema-free raw responses

--- internal/operation/definition.go
+++ internal/operation/definition.go
@@ -14,6 +14,7 @@ const (
 	ResponseJSON ResponseKind = iota
 	ResponseProblem
 	ResponseCustom
+	ResponseRaw
 )
 
 type ResponseSpec struct {
@@ -24,21 +25,21 @@ type ResponseSpec struct {
 }
 
 type Definition struct {
-	Method      string
-	UserRoute   string
-	FullRoute   string
-	InputType   reflect.Type
-	OutputType  reflect.Type
-	Invoke      func(context.Context, reflect.Value) core.ResultWriter
-	RawHandler  http.Handler
-	OperationID string
-	Tags        []string
-	Summary     string
-	Description string
-	Responses   map[int]ResponseSpec
-	Validation  bool
-	Feature     string
-	Permission  string
+	Method       string
+	UserRoute    string
+	FullRoute    string
+	InputType    reflect.Type
+	OutputType   reflect.Type
+	Invoke       func(context.Context, reflect.Value) core.ResultWriter
+	RawHandler   http.Handler
+	OperationID  string
+	Tags         []string
+	Summary      string
+	Description  string
+	Responses    map[int]ResponseSpec
+	Validation   bool
+	Feature      string
+	Permission   string
 	SecurityName string
-	Middlewares []func(http.Handler) http.Handler
+	Middlewares  []func(http.Handler) http.Handler
 }
